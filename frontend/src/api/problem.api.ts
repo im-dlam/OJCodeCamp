f58b7fc3b6ce88a8741c8147141dee02045f780b
Lam <ldl.contact.booking@gmail.com>
2026-06-20 19:19:06 +0700

    feat(admin): add problem creation UI
    - support testcase import from ZIP and JSON files
    - implement 4-step problem creation workflow

--- frontend/src/api/problem.api.ts
+++ frontend/src/api/problem.api.ts
@@ -1,4 +1,7 @@
 import type {ProblemFetchResult, ProblemDetailResult} from "../types"
+import type { 
+    ProblemCreatePayload
+ } from '../types';
 
 
 const baseURL = import.meta.env.VITE_API_URL;
@@ -26,3 +29,22 @@ export const problem_detail = async (endpoint: string): Promise<ProblemDetailRes
     const data = await res.json();
     return data; 
 }
+
+
+export const createProblemApi = async (payload: ProblemCreatePayload) => {
+  const response = await fetch(`${baseURL}/api/problems/`, { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+    credentials: "include"
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.detail || 'Error authen');
+  }
+
+  return response.json();
+};
